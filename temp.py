2a74e6219835d6e4b8e45b6d6cd75008703c91f5
rakshitmohan <81284050+rakshitmohan@users.noreply.github.com>
2022-03-21 12:19:28 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
 string user="hazRSetvQqsiW6sOb365yOzsQ8JQkOpTLnq6olQL68mL2CsPVr"
 private_key_id": "d30b0f8858589d3d1294aee5b",
 aws_key="AKIAIMNOJVGFDXXXE4OA"
+"aws_access_key_id": "AKIAIFLKOK7SWAKMOEYB",
+"aws_secret_access_key": "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"
